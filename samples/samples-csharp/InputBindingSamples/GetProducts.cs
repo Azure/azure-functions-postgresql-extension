f9ec5b10f755ce747e6525ea4e1485db35a96864
npollock14 <33988166+npollock14@users.noreply.github.com>
2023-06-12 17:38:00 -0700

    input & output binding samples

---
+++ samples/samples-csharp/InputBindingSamples/GetProducts.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.PostgreSql;
+using Microsoft.Azure.WebJobs.Extensions.PostgreSql.Samples.Common;
+
+
+namespace Microsoft.Azure.WebJobs.Extensions.PostgreSql.Samples.InputBindingSamples
+{
+    public static class GetProducts
+    {
+        [FunctionName("GetProducts")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getproducts/{cost}")]
+            HttpRequest req,
+            [PostgreSql("select * from Products where Cost = @Cost::int",
+                "PostgreSqlConnectionString",
+                parameters: "@Cost={cost}")]
+            IEnumerable<Product> products)
+        {
+            return new OkObjectResult(products);
+        }
+    }
+}
